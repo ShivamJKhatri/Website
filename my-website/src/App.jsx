efec75dcd354c802765a883e0cc1fb1352d63238
Shivam <s25khatri@uwaterloo.ca>
2026-05-25 07:25:26 -0600

    Tried getting spline working

--- my-website/src/App.jsx
+++ my-website/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <Navbar />
       <Homepage />
       <Sidebar />
-      {/* <SplineRobot /> */}
+      <SplineRobot />
     </>
   )
 }
--- my-website/src/SplineRobot.jsx
+++ my-website/src/SplineRobot.jsx
@@ -1,21 +1,28 @@
+// import Spline from '@splinetool/react-spline';
+// import { useMemo } from 'react';
+
+// export default function SplineRobot() {
+//   // Cache-bust so we always pull the latest export
+//   const sceneUrl = useMemo(
+//     () => `https://prod.spline.design/ANQ2oDIG0IimKHju/scene.splinecode?uid=${Math.random()}`,
+//     []
+//   );
+
+//   return (
+//     <div className="TrackingRobot">
+//       <Spline
+//         scene={sceneUrl}
+//         className="spline-embed"
+//         style={{ width: '100%', height: '100%' }}
+//       />
+//     </div>
+//   );
+// }
+
 import Spline from '@splinetool/react-spline';
-import { useMemo } from 'react';
 
-export default function SplineRobot() {
-  // Cache-bust so we always pull the latest export
-  const sceneUrl = useMemo(
-    () => `https://prod.spline.design/sYLsnoXIW9cD7kYf/scene.splinecode?uid=${Math.random()}`,
-    []
-  );
-
+export default function App() {
   return (
-    <div className="TrackingRobot">
-      <Spline
-        scene={sceneUrl}
-        className="spline-embed"
-        style={{ width: '100%', height: '100%' }}
-      />
-    </div>
+    <Spline scene="https://prod.spline.design/ANQ2oDIG0IimKHju/scene.splinecode" />
   );
 }
-
